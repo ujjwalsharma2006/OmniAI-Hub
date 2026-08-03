9817ffaa5232aed621c418302cf72e440a3767ad
ujjwalsharma2006 <ujjwalsharma070208@gmail.com>
2026-08-03 13:28:29 +0530

    Connect Navbar to Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,41 @@
+import Navbar from "../components/Navbar";
+
 export default function Home() {
   return (
-    <main
-      style={{
-        minHeight: "100vh",
-        background: "#0f172a",
-        color: "white",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
-    >
-      <h1 style={{ fontSize: "48px" }}>🚀 OmniAI Hub</h1>
+    <>
+      <Navbar />
 
-      <p style={{ marginTop: "15px", fontSize: "20px" }}>
-        All in One AI Platform
-      </p>
-
-      <button
+      <main
         style={{
-          marginTop: "30px",
-          padding: "14px 30px",
-          background: "#2563eb",
-          color: "#fff",
-          border: "none",
-          borderRadius: "12px",
-          fontSize: "18px",
+          minHeight: "90vh",
+          background: "#0f172a",
+          color: "white",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
         }}
       >
-        Get Started
-      </button>
-    </main>
+        <h1 style={{ fontSize: "48px" }}>🚀 OmniAI Hub</h1>
+
+        <p style={{ marginTop: "15px", fontSize: "20px" }}>
+          All in One AI Platform
+        </p>
+
+        <button
+          style={{
+            marginTop: "30px",
+            padding: "14px 30px",
+            background: "#2563eb",
+            color: "#fff",
+            border: "none",
+            borderRadius: "12px",
+            fontSize: "18px",
+          }}
+        >
+          Get Started
+        </button>
+      </main>
+    </>
   );
 }
